14190256824bd1b6374b14934e8b337b2af660ed
rlw92 <r_l_williams@yahoo.com>
2023-05-15 12:56:38 +0100

    Oval image crossfade

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
 
 let images = [1,2,3,4,5,6];
 let i =0;
+let element = document.querySelector(".slider");
 function changeSrc(){
 if(i<images.length-1){i++}
 else{i=0;}
-
+document.getElementById("test").style.opacity = "1"
 document.getElementById("test").src="media/cake"+images[i]+".jpg";
-
 }
 
-let element = document.querySelector(".slider");
 
 
-//cannot figure out how to get this to work
-function fade() {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
+function fadeout(){document.getElementById("test").style.opacity = "0.4"}
 
-
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-            },50);
-
-}
-
+setInterval(fadeout,1500)
 setInterval(changeSrc,3000)
