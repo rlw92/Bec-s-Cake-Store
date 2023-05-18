85db12b79d2f91cec324ee78be2e04deec7b0b19
rlw92 <r_l_williams@yahoo.com>
2023-05-18 18:07:44 +0100

    happy with oval crossfade

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 
-let images = [1,2,3,4,5,6];
+let images = [1,3,4,6];
 let i =0;
 let element = document.querySelector(".slider");
+
+
 function changeSrc(){
-if(i<images.length-1){i++}
-else{i=0;}
-document.getElementById("test").style.opacity = "1"
+  if(i<images.length-1){i++}
+  else{i=0;}
+
+document.getElementById("test").classList.add("fadeout");
+setTimeout(()=>{
 document.getElementById("test").src="media/cake"+images[i]+".jpg";
+setTimeout(()=>{document.getElementById("test").classList.remove("fadeout")},50)
+
+},3000)
+
 }
 
 
-
-function fadeout(){document.getElementById("test").style.opacity = "0.4"}
-
-setInterval(fadeout,1500)
-setInterval(changeSrc,3000)
+setInterval(changeSrc,6000)
